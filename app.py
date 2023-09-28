d15ce465a51ab95d21e7ac8055210e5cc80f32ef
Jaiden Reddy <jaidenreddy@gmail.com>
2023-09-28 16:31:43 -0700

    redesign app.py, gpt_helper.py to make them better structured and easier to use

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, redirect, url_for, render_template, jsonify, Response
+from flask import Flask, request, render_template, jsonify, session
 from converters import convert_to_txt
-from gpt_helper import analyze_content
+from gpt_helper import start_conversation, refine_fields
 import os
 
 UPLOAD_FOLDER = 'uploads'
@@ -8,8 +8,7 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'doc', 'docx', 'rtf', 'html'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-uploaded_files = []
+app.config['SECRET_KEY'] = os.urandom(24)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -17,48 +16,68 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # Check if the post request has the file part
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an empty file without a filename.
-        if file.filename == '':
-            return redirect(request.url)
+        file = request.files.get('file')
         if file and allowed_file(file.filename):
             filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            safe_filename = filename.rsplit('.', 1)[0] + '.txt'  # Construct .txt filename
+            safe_filename = filename.rsplit('.', 1)[0] + '.txt'
             if file.filename.rsplit('.', 1)[1].lower() != 'txt':
                 converted_content = convert_to_txt(file)
                 with open(safe_filename, 'w') as txt_file:
                     txt_file.write(converted_content)
             else:
                 file.save(safe_filename)
-
-            # Append the filename to the uploaded_files list
-            uploaded_files.append(safe_filename)
     return render_template('upload.html')
 
 @app.route('/get_files', methods=['GET'])
 def get_files():
-    # Returns a list of all the uploaded files
-    print(uploaded_files)
+    uploaded_files = [f for f in os.listdir(UPLOAD_FOLDER) if os.path.isfile(os.path.join(UPLOAD_FOLDER, f))]
     return jsonify({"files": uploaded_files})
 
 @app.route('/select_files', methods=['POST'])
 def select_files():
     selected_files = request.json.get('selected_files', [])
-    # For demonstration purposes, let's grab the first selected file's content
     first_file_content = ""
     if selected_files:
-        with open(selected_files[0], 'r') as file:
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], selected_files[0]), 'r') as file:
             first_file_content = file.read()
     return jsonify({"status": "success", "selected_files": selected_files, "first_file_content": first_file_content})
 
-@app.route('/process_message', methods=['POST'])
-def process_message():
-    user_message = request.json.get('message')
-    gpt_response = analyze_content(user_message)
-    return jsonify(gpt_response)
+@app.route('/start_conversation', methods=['POST'])
+def initiate_conversation():
+    document_content = request.json.get('document_content')
+    user_input = request.json.get('user_input')
+    session['document_content'] = document_content
+    session['confirmed_fields'] = []
+    session['rejected_fields'] = []
+    session['suggested_fields'] = []
+    session['conversation_history'] = [user_input]
+
+    response = start_conversation(document_content, user_input)
+    
+    session['suggested_fields'].extend(response['fields'])
+    session['conversation_history'].append(response["naturalResponse"])  # Add model's response to conversation history
+
+    return jsonify(response)
+
+@app.route('/refine_fields', methods=['POST'])
+def handle_field_refinement():
+    user_input = request.json.get('user_input')
+    confirmed_fields = request.json.get('confirmed_fields', [])
+    rejected_fields = request.json.get('rejected_fields', [])
+
+    # Update session's conversation history, confirmed and rejected fields
+    session['conversation_history'].append(user_input)
+    session['confirmed_fields'].extend(confirmed_fields)
+    session['rejected_fields'].extend(rejected_fields)
+    session['suggested_fields'] = list(set(session['suggested_fields']) - set(confirmed_fields + rejected_fields))
+    
+    response = refine_fields(session, user_input)
+
+    session['suggested_fields'].extend(response['fields'])
+    session['conversation_history'].append(response["naturalResponse"])  # Add model's response to conversation history
+
+    return jsonify(response)
+
 
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
--- gpt_helper.py
+++ gpt_helper.py
@@ -3,85 +3,96 @@ import json
 import time
 import os
 
-# Set the API key
 openai.api_key = os.environ.get('OPENAI_API_KEY')
+HIGH_LEVEL_OVERVIEW = "You are being used as a tool to help in a project allowing users to transform numerous pages of unstructured text data into a queryable database. In order to do this, the user uploads lots of individual files, each of the same classification. They then describe the nature of the file, and in conversation with you, they identify fields by which the file can be summarized in a database. For example, they might upload the text of an insurance report and together, you and the user might, in conversation, identify the fields of customer, company, insurance claim type, etc. Your current task: "
 
-# Pre-defined examples (we'll fill this in soon)
-examples = [
-    # Example format:
-    # {
-    #     'sales_call': 'example sales call text',
-    #     'structured_output': {
-    #         'Meeting Date': '',
-    #         'Client': '',
-    #         # ... other fields ...
-    #     }
-    # }
-]
+def start_conversation(document_content, user_input):
+    # Initial function call to identify fields
+    system_message = {
+        "role": "system",
+        "content": HIGH_LEVEL_OVERVIEW + "Read this document and the user's description of the document type. Identify possible fields by which to summarize the document."
+    }
+    user_message = {
+        "role": "user",
+        "content": user_input + " " + document_content
+    }
+    messages = [system_message, user_message]
+    
+    # Get fields and descriptions from the function call
+    fields_data = call_openai(messages, function_info)
+    
+    # Update the system message for natural language response
+    system_message["content"] = HIGH_LEVEL_OVERVIEW + "Provide a natural language response based on the identified fields and the ongoing conversation."
+    
+    # Now, create a natural language response
+    nl_response = call_openai(messages + [{"role": "assistant", "content": json.dumps(fields_data)}])
+    
+    response = {
+        "fields": fields_data.get("fields", []),
+        "descriptions": fields_data.get("descriptions", []),
+        "naturalResponse": nl_response
+    }
+    
+    return response
 
-def analyze_content(content, max_retries=1, wait_time=1):
-    messages = [
-        {
-            "role": "system",
-            "content": "You are a helpful assistant that looks at semi-structured or unstructured text pages and identifies potential fields by which they could be condensed for future use in a database. I'd like a list of fields, descriptions, and a natural language response for the conversation to continue."
-        }
-    ]
 
-    # Add the examples to the messages
-    for example in examples:
-        example['structured_output'] = json.dumps(example['structured_output'])
-        messages.extend([
-            {"role": "user", "content": example['content']},  
-            {"role": "assistant", "content": example['structured_output']}
-        ])
+def refine_fields(session_data, user_input):
+    # Build the system and user messages with session data
+    system_message = {
+        "role": "system",
+        "content": HIGH_LEVEL_OVERVIEW + "Using the document uploaded and the fields provided, refine the field suggestions based on prior interactions. Do not repeat fields that have already been confirmed or rejected."
+    }
+    
+    # Adding prior conversation and fields for context
+    conversation_history = "\n".join(session_data.get('conversation_history', []))
+    suggested_fields = ", ".join(session_data.get('suggested_fields', []))
+    confirmed_fields = ", ".join(session_data.get('confirmed_fields', []))
+    rejected_fields = ", ".join(session_data.get('rejected_fields', []))
+    document_content = session_data.get('document_content', '')
+    
+    user_message = {
+        "role": "user",
+        "content": f"{user_input} Prior conversation: {conversation_history}. Confirmed fields: {confirmed_fields}. Rejected fields: {rejected_fields}. Suggested fields: {suggested_fields}. Document content: {document_content}"
+    }
+    
+    messages = [system_message, user_message]
+    
+    # Get refined fields and descriptions from the function call
+    fields_data = call_openai(messages, function_info)
+    
+    # Update the system message for natural language response
+    system_message["content"] = HIGH_LEVEL_OVERVIEW + "Provide a natural language response based on the refined fields, prior interactions, and the ongoing conversation."
+    
+    # Now, create a natural language response with the refined fields
+    nl_response = call_openai(messages + [{"role": "assistant", "content": json.dumps(fields_data)}])
+    
+    response = {
+        "fields": fields_data.get("fields", []),
+        "descriptions": fields_data.get("descriptions", []),
+        "naturalResponse": nl_response
+    }
+    
+    return response
 
-    # Add the new content to the messages
-    messages.append({"role": "user", "content": content})
-
-    # Update the function definition to match the structured output you want
-    functions = [
-        {
-            "name": "identify_fields",
-            "description": "Identify potential fields from the provided text",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "fields": {"type": "array", "items": {"type": "string"}},
-                    "descriptions": {"type": "array", "items": {"type": "string"}},
-                    "naturalResponse": {"type": "string"}
-                },
-                "required": ["fields", "descriptions", "naturalResponse"]
-            }
-        }
-    ]
-
+def call_openai(messages, function_info=None, max_retries=1, wait_time=1):
+    functions = [function_info] if function_info else []
+    
     retries = 0
     while retries < max_retries:
         try:
-            print("Sending the following messages to OpenAI:", messages)
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo-0613",
                 messages=messages,
                 functions=functions
             )
-            print("Received response from OpenAI:", response.choices[0].message)
-            function_call = response.choices[0].message.get('function_call', {})
-            
-            # Since there isn't a real function to execute, 
-            # just return the arguments of the function call as the result.
-            if function_call:
-                arguments = json.loads(function_call['arguments'])
-                response_data = {
-                    'function_call': {
-                        'name': 'identify_fields',
-                        'arguments': arguments
-                    }
-                }
-                return response_data
-            
+            if function_info:
+                function_call = response.choices[0].message.get('function_call', {})
+                if function_call:
+                    return json.loads(function_call['arguments'])
+            else:
+                return response.choices[0].message.get('content', "")
         except json.JSONDecodeError:
-            print("Error decoding JSON. Response received:", response)
             retries += 1
             time.sleep(wait_time)
-
-    raise Exception("Maximum number of retries reached. The API may be experiencing issues.")
+    
+    raise Exception("Maximum retries reached. API issues.")
